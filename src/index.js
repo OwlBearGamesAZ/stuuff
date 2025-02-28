1824e3b25aefd104848c90d7857a437f00deb719
OwlBearGamesAZ <obcgamesaz@gmail.com>
2025-02-27 21:17:33 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-async function handler(request) {
-  const base = 'https://example.com';
-  const statusCode = 301;
 
-  const destination = new URL(request.url, base);
-  return Response.redirect(destination.toString(), statusCode);
-}
-
-// Initialize Worker
-addEventListener('fetch', event => {
-  event.respondWith(handler(event.request));
-});
